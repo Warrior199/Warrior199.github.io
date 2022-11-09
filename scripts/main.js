84dd47de344d986dd95733e18e730d4921ef9916
Warrior199 <117661915+Warrior199@users.noreply.github.com>
2022-11-09 08:53:47 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox.jpg') {
       myImage.setAttribute('src', 'images/firefox2.png');
+      alert('换了换了');
     } else {
       myImage.setAttribute('src', 'images/firefox.jpg');
+      alert('又换了又换了');
     }
 }
